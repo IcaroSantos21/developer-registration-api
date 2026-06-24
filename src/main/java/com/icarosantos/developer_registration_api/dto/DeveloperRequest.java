4933099c2ece5df32c49ffd668693a9cd0eed4ae
Icaro Santos <icaro.santosdev@gmail.com>
2026-06-24 03:15:00 -0300

    feat: add DeveloperRequest DTO

---
+++ src/main/java/com/icarosantos/developer_registration_api/dto/DeveloperRequest.java
@@ -0,0 +1,38 @@
+package com.icarosantos.developer_registration_api.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.icarosantos.developer_registration_api.model.TypeContract;
+import com.icarosantos.developer_registration_api.model.TypeDeveloper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeveloperRequest {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String enterprise;
+    private BigDecimal salary;
+    private TypeDeveloper typeDeveloper;
+    private TypeContract typeContract;
+    private LocalDate holidayDate;
+    private String cep;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private LocalDate admissionDate;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private LocalDate contractStartDate;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer contractPeriod;
+}
