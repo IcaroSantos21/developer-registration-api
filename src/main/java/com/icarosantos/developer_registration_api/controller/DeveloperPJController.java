fe3e546090bf62ab3ef3c3b5e1a1b459eaa44c8e
Icaro Santos <icaro.santosdev@gmail.com>
2026-06-25 12:33:16 -0300

    feat: add DeveloperPJController with full CRUD endpoints

---
+++ src/main/java/com/icarosantos/developer_registration_api/controller/DeveloperPJController.java
@@ -0,0 +1,49 @@
+package com.icarosantos.developer_registration_api.controller;
+
+import com.icarosantos.developer_registration_api.dto.DeveloperRequest;
+import com.icarosantos.developer_registration_api.model.DeveloperPJ;
+import com.icarosantos.developer_registration_api.service.DeveloperPJService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dev")
+@RequiredArgsConstructor
+public class DeveloperPJController {
+
+    private final DeveloperPJService developerPJService;
+
+    @PostMapping("/pj")
+    public ResponseEntity create(@RequestBody DeveloperRequest developerRequest) {
+
+        developerPJService.create(developerRequest);
+        return ResponseEntity.status(201).build();
+    }
+
+    @GetMapping("/pj")
+    public ResponseEntity<List<DeveloperPJ>> findAll() {
+        return ResponseEntity.ok(developerPJService.findAll());
+    }
+
+    @GetMapping("/pj/{id}")
+    public ResponseEntity<DeveloperPJ> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(developerPJService.findById(id).get());
+    }
+
+    @PutMapping("/pj/{id}")
+    public ResponseEntity update(@PathVariable Long id, @RequestBody DeveloperRequest developerRequest) {
+        developerPJService.update(id, developerRequest);
+        return ResponseEntity.status(200).build();
+    }
+
+    @DeleteMapping("/pj/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        developerPJService.delete(id);
+        return ResponseEntity.status(204).build();
+    }
+
+
+}
